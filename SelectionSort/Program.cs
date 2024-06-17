960ec1db98f60d23885fb99c9ffeb1423ab09285
Coders9614 <joonyong.kim1@outlook.com>
2024-06-17 11:05:54 -0600

    Add project files.

---
+++ SelectionSort/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SelectionSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 77, 92, 88, 67, 82 };
+
+            Console.WriteLine("The orignal scores:");
+            Display(arr);
+
+            SelectionSort(arr);
+
+            Console.WriteLine("The Assending scores:");
+            Display(arr);
+            Console.ReadKey();
+        }
+        static void SelectionSort(int[] arr )
+        {
+            int n = arr.Length;
+            for( int i = 0;i < n; i++ )
+            {
+                 int midscore =i;
+                for( int j = 0;j <n; j++ )
+                {
+                    if (arr[j] < arr[midscore])
+                    {  midscore = j; }
+                }
+                int temp = arr[midscore];
+                arr[midscore] = arr[i];
+                arr[i] = temp;
+            }
+          
+        }
+       static void Display(int[] arr)
+        {
+            foreach(int score in arr )
+            {
+                Console.Write(score + " ");
+            }
+            Console.WriteLine();
+        }
+ 
+    }
+}
